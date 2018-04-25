6fff21095e5ddbd8fb5f7da8c7c5fe67cd5dc495
alexis <alexis@romero>
2018-04-25 19:22:48 -0300

    heroku

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     super();
     this.state = {
       todos
-    };
+    }; //comentario extra para git 
     this.handleAddTodo = this.handleAddTodo.bind(this);
   }
 
